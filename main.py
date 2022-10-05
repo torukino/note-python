e2f9abbbf0f96fad722c6e53ac972bf9480db5e9
torukino <toruk0131@gmail.com>
2022-10-06 01:36:31 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import json
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def Hello():
+    return [{"karteNo":"01","content":"三鷹市"},{"karteNo":"02","content":"武蔵野市"}]
